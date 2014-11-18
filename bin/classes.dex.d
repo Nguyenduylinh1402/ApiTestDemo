44d0fe6672b61a7f29701e479a397a5afb4f9472
unknown <linhduyng01@gmail.com>
2014-11-18 08:13:42 +0700

    Api Test Demo

---
+++ bin/classes.dex.d
@@ -0,0 +1,4 @@
+C:\Users\Steve\AutomationTest\ApiDemoTest\bin\classes.dex : \
+C:\Users\Steve\AutomationTest\ApiDemoTest\bin\classes\com\linhnguyen\test\ApiDemosTest.class \
+C:\Users\Steve\AutomationTest\ApiDemoTest\bin\classes\com\linhnguyen\test\Logger.class \
+C:\Users\Steve\AutomationTest\ApiDemoTest\bin\classes\com\linhnguyen\test\UiAutomatorUtils.class \
